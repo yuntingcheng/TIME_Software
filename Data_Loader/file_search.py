50c7cd74fc39f4da48b6b0884ad0cbd6d6e6c3be
Victoria Butler <vlb9398@g.rit.edu>
2018-09-06 15:51:55 -0400

    seems to be working

--- Data_Loader/file_search.py
+++ Data_Loader/file_search.py
@@ -1,19 +1,38 @@
 import netCDF4
-import sys, os
+import sys, os, re
 
 dir = '/Users/vlb9398/Dropbox/NetCDF_Test/'
 sys.path.append(dir)
 
+keep = []
 def main():
-    if os.path.exists(dir):
-        files = [dir + x for x in os.listdir(dir) if (x.startswith("raw") and x.endswith(".nc"))]
-        newest = max(files , key = os.path.getctime)
-        oldest = min(files, key = os.path.getctime)
-        print("Available Files:",'\n', "Newest: %s" %(newest.replace(dir,'')),'\n', "Oldest: %s" %(oldest.replace(dir,'')))
-        
+    print("Choose name search or date search: ")
+    action = input("a : name , b : date")
+    print("\n")
 
-    else :
-        print("Directory Not Found")
+    if action == 'b':
+        if os.path.exists(dir):
+            files = [dir + x for x in os.listdir(dir) if (x.startswith("raw") and x.endswith(".nc"))]
+            newest = max(files , key = os.path.getctime)
+            oldest = min(files, key = os.path.getctime)
+            print("Available Files:",'\n', "Newest: %s" %(newest.replace(dir,'')),'\n', "Oldest: %s" %(oldest.replace(dir,'')))
+            action = input("Enter First Date: [t0,t1] ")
+            print("\n")
+            action2 = input("Enter Second Date: [y0,t1] ")
+            print("\n")
+
+            files2 = [dir + y for y in os.listdir(dir) if (re.search(action,y)) or (re.search(action2,y))]
+            for s in files :
+                if os.path.getctime(s) <= os.path.getctime(files2[0]) \
+                and os.path.getctime(s) >= os.path.getctime(files[1]) :
+                    keep.append(s)
+
+                else :
+                    print("No files found in that range. Please try again...")
+
+            print(keep)
+        else :
+            print("Directory Not Found")
 
 if __name__ == "__main__" :
     main()
