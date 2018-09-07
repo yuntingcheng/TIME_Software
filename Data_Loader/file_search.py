db8990ba372fc11b464c1a8af234d57493084a06
Victoria Butler <vlb9398@g.rit.edu>
2018-09-07 13:45:31 -0400

    all pieces working now

--- Data_Loader/file_search.py
+++ Data_Loader/file_search.py
@@ -1,36 +1,38 @@
 import netCDF4
 import sys, os, re
 
+# ------------- NetCDF Directory ----------------
 dir = '/Users/vlb9398/Dropbox/NetCDF_Test/'
 sys.path.append(dir)
+# -----------------------------------------------
 
 keep = []
 files2 = []
 files = []
+
 # ===================================================================================================
 def main():
     print("Choose name search or date search: ")
-    action = input("a : name , b : date")
-    print("\n")
+    action = input("a : name , b : date \n")
 
     if action == 'a':
-        name_search(files2)
+        name_search(files2,action)
     elif action == 'b':
-        date_search(files)
+        date_search(files,action)
     else :
         print("I'm sorry... I didn't understand. Please try again.")
 # ====================================================================================================
-def name_search(files2):
-    action4 = input('Enter name to search: ')
-    print("")
+def name_search(files2,action):
+    action4 = input('Enter name to search: \n')
     if action == 'a':
         if os.path.exists(dir):
             files = [dir + x for x in os.listdir(dir) if (x.startswith(action4) and x.endswith(".nc"))]
-            print(files)
+            print('\n'.join([s.replace(dir,'') for s in files]))
             print('Do you still want to search by time?')
-            action5 = ('y : yes , n : no')
+            action5 = input('y : yes , n : no \n')
+
             if action5 == 'y':
-                date_search(files)
+                date_search(files,action)
             elif action5 == 'n':
                 pass
             else :
@@ -41,23 +43,21 @@ def name_search(files2):
             print("Directory does not exist")
 
     elif action == 'b':
-        keep = [x for x in files2 if re.search(action5,x)]
-        print(keep)
+        keep = [x for x in files2 if re.search(action4,x)]
+        print('\n'.join([s.replace(dir,'') for s in keep]))
 
     else :
         print('uh-oh...something went wrong...')
 # ======================================================================================================
-def date_search(files):
+def date_search(files, action):
     if action == 'b' :
         if os.path.exists(dir):
-            files = [dir + x for x in os.listdir(dir) if x.endswith(".nc")]
+            files = [dir + x for x in os.listdir(dir) if (x.endswith(".nc") and x.startswith("raw"))]
             newest = max(files , key = os.path.getctime)
             oldest = min(files, key = os.path.getctime)
             print("Available Files:",'\n', "Newest: %s" %(newest.replace(dir,'')),'\n', "Oldest: %s" %(oldest.replace(dir,'')))
-            action2 = input("Enter Oldest Date: ")
-            print("\n")
-            action3 = input("Enter Newest Date: ")
-            print("\n")
+            action2 = input("Enter Oldest Date: \n")
+            action3 = input("Enter Newest Date: \n")
 
             files2 = [dir + y for y in os.listdir(dir) if (re.search(action2,y)) or (re.search(action3,y))]
             for s in files :
@@ -71,10 +71,10 @@ def date_search(files):
 
             print(keep)
             print("Do you still want to search by name?")
-            action4 = input('y : yes , n : no')
+            action4 = input('y : yes , n : no \n')
 
             if action4 == 'y':
-                name_search(keep)
+                name_search(keep,action)
             elif action4 == 'n':
                 pass
             else :
@@ -82,20 +82,19 @@ def date_search(files):
                 pass
 
     elif action == 'a' :
-        action6 = input("Enter Oldest Date: ")
-        print("")
-        action7 = input("Enter Newest Date: ")
-        print("")
+        action6 = input("Enter Oldest Date: \n")
+        action7 = input("Enter Newest Date: \n")
 
-        files2 = [for s in files if (re.search(action6,y)) or (re.search(action7,y))]
+        files2 = [ y for y in files if (re.search(action6,y)) or (re.search(action7,y))]
         for s in files :
             if os.path.getctime(s) <= os.path.getctime(files2[0]) \
             and os.path.getctime(s) >= os.path.getctime(files2[1]) :
                 keep.append(s)
-        print(keep)
             else :
                 print("No files found in that range.")
                 pass
+
+        print('\n'.join([s.replace(dir,'') for s in keep]))
     else :
         print("Action not recognized")
 # =======================================================================================================
