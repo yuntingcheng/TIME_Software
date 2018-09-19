2880fc7eee82f6bba9909c3ce2c400da0aead1c0
Victoria Butler <vlb9398@g.rit.edu>
2018-09-19 12:18:35 -0400

    timer check in sftp

--- sftp/mce1_sftp.py
+++ sftp/mce1_sftp.py
@@ -3,12 +3,14 @@ import subprocess
 import time
 import shutil
 import sys
+import datetime as dt
 
 def main():
     dir = '/data/cryo/current_data/'
     print('------- Starting Data Transfer -------')
     a = 0
-    while True:
+    begintimer = dt.datetime.utcnow()
+    while endtime - begintimer < dt.timedelta(seconds=5):
         if a == 0:
             subprocess.Popen(['scp', '/data/cryo/current_data/temp.run', 'time@time-master.caltech.edu:/home/time/Desktop/time-data/mce1/temp.run']).wait()
             subprocess.Popen(['rm %s' % ('/data/cryo/current_data/temp.run')],shell=True)
@@ -19,12 +21,14 @@ def main():
                 subprocess.Popen(['rm %s' % (mce_file_name)],shell=True)
                 print('File Transfered :' , mce_file_name.replace(dir,''))
                 a += 1
+                begintimer = dt.datetime.now()
 
             else :
-                print("File Doesn't Exist (2)")
+                print("File Doesn't Exist")
         else :
             pass
     else :
+        print('File Transfer Stopped')
         sys.exit()
 
 if __name__ == '__main__':
