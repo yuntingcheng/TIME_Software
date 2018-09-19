1b06988c8ad06b62bd1d235270bc34da4a83146d
Victoria Butler <vlb9398@g.rit.edu>
2018-09-19 17:45:23 -0400

    nc classic formatting changed for strings

--- main/netcdf.py
+++ main/netcdf.py
@@ -4,7 +4,7 @@ import os
 import sys
 #import takedata_test as td
 import datetime as now
-from netCDF4 import num2date, date2num
+import numpy as np
 
 
 tempfiledir = '/home/time/Desktop/time-data/netcdffiles'
@@ -19,10 +19,10 @@ def new_file(h_size, head, filestarttime):
 
      # GUI PARAMETERS ---------------------------------------------------------------------------------
     mce.createDimension('det',1)
-    mce.createDimension('obs',1)
-    mce.createDimension('date',1)
-    mce.createDimension('f',1)
-    mce.createDimension('mode',1)
+    mce.createDimension('obs',3)
+    mce.createDimension('date',26)
+    mce.createDimension('f',8)
+    mce.createDimension('mode',2)
     mce.createDimension('r',1)
     mce.createDimension('t',None)
     # Dimensions for Data Arrays -------------------------------------------------------------------
@@ -35,14 +35,14 @@ def new_file(h_size, head, filestarttime):
 
 
     # creating variables --------------------------------------------------------------------------------
-    Observer = mce.createVariable("observer","S3","obs")
-    Datetime = mce.createVariable('datetime', 'S26','date')
-    Frames = mce.createVariable('frames', 'S8','f')
-    Datamode = mce.createVariable('datamode','S2','mode')
+    Observer = mce.createVariable("observer","S1","obs")
+    Datetime = mce.createVariable('datetime', 'S1','date')
+    Frames = mce.createVariable('frames', 'S1','f')
+    Datamode = mce.createVariable('datamode','S1','mode')
     Detector = mce.createVariable('detector','f8','det')
     Rc = mce.createVariable('rc','S1','r') # can either use rc name or integer used by gui
     global Time
-    Time = mce.createVariable('time','S26','t')
+    Time = mce.createVariable('time','S1','t')
 
     global Rms_Noise_All
     global Rms_Noise
@@ -54,16 +54,16 @@ def new_file(h_size, head, filestarttime):
     Raw_Data = mce.createVariable('raw_data','f8',('t','raw_rows','raw_cols','raw_num'))
     Raw_Data_All = mce.createVariable('raw_data_all','f8',('t','raw_rows','raw_cols_all','raw_num'))
 
-    global Header
-    Header = mce.createVariable('header','S3',('t','v','k'))
+    # global Header
+    # Header = mce.createVariable('header','S3',('t','v','k'))
 
     parafilename = ('tempfiles/tempparameters.txt')
     parafile = open(parafilename, 'r')
     parameters = parafile.readline().strip().split()
-    Observer[0] = parameters[0]
-    Frames[0] = parameters[3]
-    Datamode[0] = parameters[1]
-    Rc[0] = parameters[2]
+    Observer[0] = nc.stringtochar(np.array[parameters[0]],'S3')
+    Frames[0] = nc.stringtochar(np.array[parameters[3]],'S8')
+    Datamode[0] = nc.stringtochar(np.array[parameters[1]],'S2')
+    Rc[0] = nc.stringtochar(np.array[parameters[2]],'S1')
     parafile.close()
 
     mce.close()
@@ -71,14 +71,14 @@ def new_file(h_size, head, filestarttime):
 
 def data_all(h,a,head,filestarttime):
     mce = Dataset(tempfiledir + "/mce1_%s.nc" %(filestarttime),"a")
-    Time[a] = str(now.datetime.utcnow())
+    Time[a] = nc.stringtochar(np.array[str(now.datetime.utcnow())],'S26')
     Raw_Data_All[a,:,:,:] = h
-    Header[a,:,:] = head
+    #Header[a,:,:] = head
     mce.close()
 
 def data(h,a,head,filestarttime):
     mce = Dataset(tempfiledir + "/mce1_%s.nc" %(filestarttime),"a")
-    Time[a] = str(now.datetime.utcnow())
+    Time[a] = nc.stringtochar(np.array[str(now.datetime.utcnow())],'S26')
     Raw_Data[a,:,:,:] = h
-    Header[a,:,:] = head
+    #Header[a,:,:] = head
     mce.close()
