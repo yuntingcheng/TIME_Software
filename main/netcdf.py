e3b2fb67378cb025325cbd67cc2f4f61b411b2cd
Victoria Butler <vlb9398@g.rit.edu>
2018-09-20 16:41:09 -0400

    new data appending method

--- main/netcdf.py
+++ main/netcdf.py
@@ -73,8 +73,8 @@ def new_file(h_size, head, filestarttime):
 
 def data_all(h,a,head,filestarttime):
     mce = nc.Dataset(tempfiledir + "/mce1_%s.nc" %(filestarttime),"a")
-    Time[a,:] = np.array([str(now.datetime.utcnow())],dtype='S26')
-    Raw_Data_All[a,:,:,:] = h
+    Time[:] = np.array([str(now.datetime.utcnow())],dtype='S26')
+    Raw_Data_All[:] = h
     print Raw_Data_All.shape
     #new_head = np.array([head],dtype='S3')
     #print new_head
@@ -83,7 +83,7 @@ def data_all(h,a,head,filestarttime):
 
 def data(h,a,head,filestarttime):
     mce = nc.Dataset(tempfiledir + "/mce1_%s.nc" %(filestarttime),"a")
-    Time[a,:] = np.array([str(now.datetime.utcnow())],dtype='S26')
+    Time[:] = np.array([str(now.datetime.utcnow())],dtype='S26')
     #Raw_Data[a,:,:,:] = h
     #new_head = np.array([head],dtype='S3')
     #print new_head
