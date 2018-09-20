6d97aafd4eb93f64a3a1db51d994839c066c366c
Victoria Butler <vlb9398@g.rit.edu>
2018-09-20 17:18:02 -0400

    same 3

--- main/netcdf.py
+++ main/netcdf.py
@@ -76,7 +76,7 @@ def data_all(h,a,head,filestarttime):
     Time[a,:] = np.array([str(now.datetime.utcnow())],dtype='S26')
     #Raw_Data_All[a,:,:,:] = h
     #print Raw_Data_All.shape
-    new_head = np.array([head],dtype='S15')
+    new_head = np.array([head],dtype='S15').reshape((2,16))
     Header[a,:,:] = new_head
     mce.close()
 
