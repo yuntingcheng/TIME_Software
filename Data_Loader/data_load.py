2829a581fc9b35275ac3c669e4845866d1f81414
Victoria Butler <vlb9398@g.rit.edu>
2018-09-20 15:31:36 -0400

    update file search for no groups

--- Data_Loader/data_load.py
+++ Data_Loader/data_load.py
@@ -3,12 +3,10 @@ from netCDF4 import Dataset
 from netCDF4 import MFDataset
 import sys
 
-def all_files(file,group,var):
+def all_files(file,var):
     f = MFDataset(file,'r')
-
-    s = f.groups[group]
-    t = s.variables[var][:]
-    print(t[0:10])
+    t = f.variables[var][:]
+    print(t[:])
 
     print("Do you need to load another dataset?")
     action3 = input('y : yes , n : no \n')
@@ -17,12 +15,10 @@ def all_files(file,group,var):
     else :
         print("Have a nice day")
 
-def one_file(file,group,var):
+def one_file(file,var):
     f = Dataset(file[0],'r')
-
-    s = f.groups[group]
-    t = s.variables[var][:]
-    print(t[0:10])
+    t = f.variables[var][:]
+    print(t[:])
 
     print("Do you need to load another dataset?")
     action4 = input('y : yes , n : no \n')
@@ -35,26 +31,17 @@ def file_keys(file,num):
     print(file[0])
     f = Dataset(file[0],'r')
 
-    print("Group Keys:")
-    print(f.groups.keys())
-    action = input("Pick a GROUP \n")
-
     print("Variable Keys:")
-    print(f.groups[action].variables.keys())
-    action1 = input("Pick a VARIABLE \n")
+    print(f.variables.keys())
+    action = input("Pick a VARIABLE \n")
 
-    v = f.groups[action]
     print("================================")
-    print("Meta Data for {group}".format(group=action))
-    print(v)
-    print("================================")
-    print("================================")
-    print("Meta Data for {var}".format(var = action1))
-    print(v.variables[action1])
+    print("Meta Data for {var}".format(var = action))
+    print(f.variables[action])
     print("================================")
 
     if num == 'one':
-        one_file(file,action,action1)
+        one_file(file,action)
     else :
-        all_files(file,action,action1)
+        all_files(file,action)
 #------------------------------------------------------------------------------
