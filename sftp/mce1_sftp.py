51921fdeb667b4c6e821226570c8cb6da048ec38
Victoria Butler <vlb9398@g.rit.edu>
2018-09-21 16:57:03 -0400

    still getting a partial frame warning

--- sftp/mce1_sftp.py
+++ sftp/mce1_sftp.py
@@ -15,10 +15,10 @@ def main():
         if os.path.exists("/data/cryo/current_data/temp.%0.3i" %(a+1)) :
             mce_file_name = '/data/cryo/current_data/temp.%0.3i' %(a)
             if a == 0:
-                subprocess.call(['scp', '/data/cryo/current_data/temp.run', 'time@time-master.caltech.edu:/home/time/Desktop/time-data/mce1/temp.run'])
+                subprocess.Popen(['scp', '/data/cryo/current_data/temp.run', 'time@time-master.caltech.edu:/home/time/Desktop/time-data/mce1/temp.run']).wait()
                 #subprocess.Popen(['rm %s' % ('/data/cryo/current_data/temp.run')],shell=True)
             if os.path.exists(mce_file_name):
-                subprocess.call(['scp', mce_file_name, 'time@time-master.caltech.edu:/home/time/Desktop/time-data/mce1/temp.%0.3i' % (a)])
+                subprocess.Popen(['scp', mce_file_name, 'time@time-master.caltech.edu:/home/time/Desktop/time-data/mce1/temp.%0.3i' % (a)]).wait()
                 subprocess.Popen(['rm %s' % (mce_file_name)],shell=True)
                 print 'File Transfered :' , mce_file_name.replace(dir,'')
                 a += 1
