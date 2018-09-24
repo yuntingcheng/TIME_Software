aa34ac86b81fa1b8cd94e671da381944e44a2736
Victoria Butler <vlb9398@g.rit.edu>
2018-09-24 19:02:27 -0400

    adding back in rms data

--- main/netcdf.py
+++ main/netcdf.py
@@ -71,7 +71,7 @@ def new_file(h_size, head, filestarttime):
     mce.close()
     return mce
 
-def data_all(h,n,head,filestarttime):
+def data_all(h,d,n,head,filestarttime):
     mce = nc.Dataset(tempfiledir + "/mce1_%s.nc" %(filestarttime),"a")
     Time[n,:] = np.array([str(now.datetime.utcnow())],dtype='S26')
     Raw_Data_All[n,:,:,:] = h
@@ -80,10 +80,10 @@ def data_all(h,n,head,filestarttime):
     #Header[a,:,:] = new_head
     mce.close()
 
-def data(h,n,head,filestarttime):
+def data(h,d,n,head,filestarttime):
     mce = nc.Dataset(tempfiledir + "/mce1_%s.nc" %(filestarttime),"a")
     Time[n,:] = np.array([str(now.datetime.utcnow())],dtype='S26')
-    #Raw_Data[a,:,:,:] = h
+    Raw_Data[a,:,:,:] = h
     #new_head = np.array([head],dtype='S3')
     #print new_head
     #Header[a,:,:] = new_head
