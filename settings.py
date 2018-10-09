493bed33b3997108a787b59fbb1ada64f9f49445
Victoria Butler <vlb9398@g.rit.edu>
2018-10-09 16:50:59 -0400

    added full textfile path

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 def mode_check(m):
     print('mode check is called false')
-    f = open('tempfiles/data_send.txt','w')
+    f = open('/home/pilot2/TIME_Software/tempfiles/data_send.txt','w')
     f.write('False')
     f.close()
