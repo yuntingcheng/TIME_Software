c6c53497249a604f57054ee5f412abad8e8dff26
Victoria Butler <vlb9398@g.rit.edu>
2018-10-09 21:46:51 -0400

    changed mode_check and tel_sock data_send checker

--- settings.py
+++ settings.py
@@ -1,5 +1,15 @@
 def mode_check(m):
-    print('mode check is called false')
-    f = open('/home/pilot2/TIME_Software/tempfiles/data_send.txt','w')
-    f.write('False')
-    f.close()
+    if m == False :
+        print('mode check is called false')
+        f = open('/home/pilot2/TIME_Software/tempfiles/data_send.txt','w')
+        f.write('False')
+        f.close()
+
+    elif m == True :
+        f = open('/home/pilot2/TIME_Software/tempfiles/data_send.txt','w')
+        f.write('True')
+        f.close()
+
+    else :
+        f = open('/home/pilot2/TIME_Software/tempfiles/data_send.txt','w')
+        f.close()
