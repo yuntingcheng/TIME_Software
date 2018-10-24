287137142a277e63be73918be214cf94a08f8bff
Victoria Butler <vlb9398@g.rit.edu>
2018-10-24 16:43:28 -0400

    wasn't recognizing new line character

--- Beta0.2/pyqtgui.py
+++ Beta0.2/pyqtgui.py
@@ -512,13 +512,13 @@ class mcegui(QtGui.QWidget):
         print(self.n_files)
         #----------------------------------------------------------------------------------
         # start the mce1 file system check (rit mce)
-        sftp1 = ['sshpass -p "time-pilot2" ssh -o StrictHostKeyChecking=no\
+        sftp1 = ['sshpass -p "time-pilot2" ssh -o StrictHostKeyChecking=no
             pilot2@timemce.rit.edu ; python /Desktop/mce1_sftp.py]
         process = subprocess.Popen(sftp1,stdout=subprocess.PIPE, shell=True)
         proc_stdout = process.communicate()[0].strip()
         #----------------------------------------------------------------------------------
         # start the mce2 file system check (caltech mce)
-        sftp2 = ['sshpass -p "CII@zof7" ssh -o StrictHostKeyChecking=no\
+        sftp2 = ['sshpass -p "CII@zof7" ssh -o StrictHostKeyChecking=no
             time@time-mce-0.caltech.edu ; python /Desktop/mce2_sftp.py]
         process = subprocess.Popen(sftp2,stdout=subprocess.PIPE, shell=True)
         proc_stdout = process.communicate()[0].strip()
